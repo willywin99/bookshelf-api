72287bd8061d8a7e4f2b8b90935386337eebc827
willywin99 <willywinsianturi@gmail.com>
2022-03-16 06:24:45 +0700

    Last Criteria done :))

--- src/handler.js
+++ src/handler.js
@@ -188,9 +188,34 @@ const editBookByIdHandler = (request, h) => {
   return response;
 };
 
+const deleteBookByIdHandler = (request, h) => {
+  const {bookId} = request.params;
+
+  const index = books.findIndex((b) => b.id === bookId);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+    response.code(200);
+    // return index;
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  // return index;
+  return response;
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
   editBookByIdHandler,
+  deleteBookByIdHandler,
 };
